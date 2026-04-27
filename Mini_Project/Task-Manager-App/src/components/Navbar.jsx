84ebeabd1d14ec1284e5c695bf65be32611e55b9
X0Sh4dE <shubham260804@gmail.com>
2026-04-27 19:43:23 +0530

    Added Output Screenshots

--- Mini_Project/Task-Manager-App/src/components/Navbar.jsx
+++ Mini_Project/Task-Manager-App/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar({ search, setSearch }) {
       <h1 className="text-xl font-bold p-3">Task Manager</h1>
       <input
         placeholder="Search..."
-        className="p-4 bg-gray-700 w-3xs"
+        className="p-2 bg-gray-700 w-3xs rounded-sm"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
